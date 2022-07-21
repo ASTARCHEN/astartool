09d83a69953c4770899d284a9772886b69234a61
A.Star <astar@snowland.ltd>
2022-07-21 22:43:34 +0800

    fix: _string增加了force_str以增加对django的适配

--- astartool/string/_string.py
+++ astartool/string/_string.py
@@ -105,6 +105,28 @@ def force_bytes(s, encoding='utf-8', errors='strict'):
         return s.encode(encoding, errors)
 
 
+def force_str(s, encoding='utf-8', strings_only=False, errors='strict'):
+    """
+    Similar to smart_str(), except that lazy instances are resolved to
+    strings, rather than kept as lazy objects.
+
+    If strings_only is True, don't convert (some) non-string-like objects.
+    """
+    # Handle the common case first for performance reasons.
+    if issubclass(type(s), str):
+        return s
+    if strings_only and is_protected_type(s):
+        return s
+    try:
+        if isinstance(s, bytes):
+            s = str(s, encoding, errors)
+        else:
+            s = str(s)
+    except UnicodeDecodeError as e:
+        raise DjangoUnicodeDecodeError(s, *e.args)
+    return s
+
+
 def to_text(value, encoding='utf-8'):
     """Convert value to unicode, default encoding is utf-8
 
